d05f876b512a467d18fcecec01895f85811da9e6
joseph <joe.wojtowicz@gmail.com>
2020-12-04 17:15:13 -0500

    crazy

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static('public'));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
 
 
 app.listen(PORT, () => {
