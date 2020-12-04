b41f93cc26348c08386008c6d55b32da6f327810
joseph <joe.wojtowicz@gmail.com>
2020-12-04 15:19:21 -0500

    update

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -107,16 +107,16 @@ const renderNoteList = (notes) => {
 
   const noteListItems = [];
 
-  // Returns jquery object for li with given text and delete button
-  // unless withDeleteButton argument is provided as false
-  const create$li = (text, withDeleteButton = true) => {
-    const $li = $("<li class='list-group-item'>");
-    const $span = $("<span>").text(text);
-    $li.append($span);
+// Returns jquery object for li with given text and delete button
+// unless withDeleteButton argument is provided as false
+const create$li = (text, withDeleteButton = true) => {
+  const $li = $("<li class='list-group-item'>");
+  const $span = $("<span>").text(text);
+  $li.append($span);
 
-    if (withDeleteButton) {
-      const $delBtn = $(
-        "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+  if (withDeleteButton) {
+    const $delBtn = $(
+     "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
       );
       $li.append($delBtn);
     }
